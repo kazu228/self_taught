7778d91c1241a9a314e110faaae98af3bcaaf56f
kuroneko <kskkm0228@gmail.com>
2019-10-01 08:16:40 +0900

    add challenge3.py

---
+++ challenge3.py
@@ -0,0 +1,42 @@
+# 1
+print("hello")
+print("kazuaki")
+print("How are you?")
+
+#2
+number = 9
+
+if number <= 10:
+    print("OK")
+else:
+    print("NG")
+
+# 3
+
+num = 22
+if num <= 10:
+    print("10以下")
+elif  num >= 10 and num <= 25:
+    print("10以上、25以下")
+
+elif num >= 25:
+    print("25以上")
+
+# 4
+result = 100 % 3
+print(result)
+
+# 6
+
+result = 100 / 3
+print(result)
+
+# 7
+
+age = 21
+
+if age >= 20:
+    print("成人です。")
+else:
+    print("未成人です。")
+

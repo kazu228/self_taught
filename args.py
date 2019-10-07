68b9274b87cca9eb842e65c7a9d975a7be12c2e3
kuroneko <kskkm0228@gmail.com>
2019-10-07 12:37:57 +0900

    **kwargsを使ってみた。

--- args.py
+++ args.py
@@ -1,4 +1,4 @@
-
+# *args
 def sum(*args):
     total = 0
     for i in args:
@@ -7,3 +7,13 @@ def sum(*args):
 
 print(sum(1, 2, 3, 4, 5, 6, 7, 8, 9,))
 print(sum(2, 5, 6))
+
+# **kwargs
+
+def  bar(**kwargs):
+    for x in kwargs:
+        print(x)
+        print(kwargs[x])
+    print(kwargs)
+
+bar(x=10, y=20, z="Python")

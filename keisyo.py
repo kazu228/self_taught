eda085ec82e36cecd9c53c43f190a3aa1cbb92af
kuroneko <kskkm0228@gmail.com>
2019-10-07 15:56:56 +0900

    super().__init__()、親クラスの初期化メソッドを子クラスに取り込む

--- keisyo.py
+++ keisyo.py
@@ -1,8 +1,9 @@
 
 import turtle
-# ttributeError: 'Kame' object has no attribute 'screen'やられた！！　親クラスの__init__を子クラスの__init__が上書きしてしまう。
+# ttributeError: 'Kame' object has no attribute 'screen'やられた！！　親クラスの__init__のすべてを子クラスの__init__が上書きしてしまう。
 class Kame(turtle.Turtle):
     def __init__(self):
+        super().__init__()  # 親クラスの__init__()をすべて呼び出してからshape()を書き換える super()は、親クラス
         self.shape('turtle')
 
 kame_test = Kame()

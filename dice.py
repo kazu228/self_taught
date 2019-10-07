bf2aa60d662621577ca1fa7db8e1bf7a075582cd
kuroneko <kskkm0228@gmail.com>
2019-10-07 15:19:43 +0900

    エラー処理

--- dice.py
+++ dice.py
@@ -2,6 +2,8 @@ import random
 
 class Dice:
     def __init__(self, face_num=6):
+        if face_num not in [4,6,8,12,20]:
+            raise Exception('そんな正多面体はありません。')
         self.face_num = face_num 
 
     def shoot(self):
